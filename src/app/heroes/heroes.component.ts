d573c6f7c2b39c7622d793943a2fe7551300dabd
humbamp123 <humb.amp123@gmail.com>
2024-04-13 21:49:14 -0700

    add list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,17 +2,20 @@ import { Component } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
 import { Hero } from '../hero';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [UpperCasePipe, FormsModule],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css',
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heros = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
