268a0a276ebaf300da271cc0ddc67966064e5026
humbamp123 <humb.amp123@gmail.com>
2024-04-13 23:49:43 -0700

    add hero, message service, and messages component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,33 @@
 import { Component } from '@angular/core';
-import { UpperCasePipe } from '@angular/common';
 import { Hero } from '../hero';
-import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule],
+  imports: [HeroDetailComponent],
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  heros = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected Hero id=${hero.id}`);
   }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 }
